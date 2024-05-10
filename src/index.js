a7063efcc7b9e9e8dcd9930e7e5d75ef4ff814ab
yeralShaik <yeraldinshaik@gmail.com>
2024-05-10 14:39:33 -0500

    agregando api para electronic products, corrigiendo medidas

--- src/index.js
+++ src/index.js
@@ -14,7 +14,19 @@ fetch('https://fakestoreapi.com/products?limit=3')
             })
 
 
-
+fetch('https://fakestoreapi.com/products/category/electronics?limit=3')
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error('error de red:' + response.statusText)
+                }
+                return response.json()
+            })
+            .then((data) => {
+              getElectronicsProducts(data)
+            })
+            .catch((error) => {
+              console.error('Error:', error)
+            })
 
     function getAllProduct(products) {
     const allproductsContainer = document.querySelector('.allproducts-section')
@@ -41,36 +53,89 @@ products.forEach((product) => {
         productPrices.className = 'product-prices'
         productPrices.innerText = `$${product.price.toFixed(2)}`;
 
-        /*<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0M8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5z"/>
-</svg>*/
-        const cartIcon = document.createElement('svg');
-        cartIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-        cartIcon.setAttribute('width', '18');  
-        cartIcon.setAttribute('height', '16');  
-        cartIcon.setAttribute('fill', 'currentColor');  
-        cartIcon.setAttribute('class', 'bi bi-cart');  
-        cartIcon.setAttribute('viewBox', '0 0 16 16');  
+
+        const cartIcon = document.createElement('svg')
+        cartIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+        cartIcon.setAttribute('width', '18')
+        cartIcon.setAttribute('height', '16') 
+        cartIcon.setAttribute('fill', 'currentColor')
+        cartIcon.setAttribute('class', 'bi bi-bag-plus')
+        cartIcon.setAttribute('viewBox', '0 0 16 16')
     
-        const path = document.createElement('path');
+        const path = document.createElement('path')
         path.setAttribute(
           'd',
-          'M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 a 2 2 0 0 0 0-4m7 0a 2 2 0 1 0 0 4 a 2 2 0 0 0 0-4'
+          'M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z'
         );
     
-        cartIcon.appendChild(path);
+        cartIcon.appendChild(path)
     
         // Agregar el ícono al elemento de precios
-        productPrices.appendChild(cartIcon);
+        productPrices.appendChild(cartIcon)
 
  
-        productCard.appendChild(productImg);
-        productCard.appendChild(productCategory);
-        productCard.appendChild(productName);
-        productCard.appendChild(productPrices);
+        productCard.appendChild(productImg)
+        productCard.appendChild(productName)
+        productCard.appendChild(productCategory)
+        productCard.appendChild(productPrices)
 
     allproductsContainer.appendChild(productCard)
 
+})
+} 
+
+
+function getElectronicsProducts(products) {
+    const electronicsProductsContainer = document.querySelector('.allelectronicproducts-section')
+       electronicsProductsContainer.innerHTML = ''
+
+products.forEach((product) => {
+    const productCard = document.createElement('div')
+    productCard.className = 'product-card'
+
+const productImg = document.createElement('img')
+    productImg.src = product.image   
+    productImg.alt = product.title
+
+const productCategory = document.createElement('p')
+    productCategory.className = 'product-category'
+    productCategory.innerText = product.category
+
+const productName = document.createElement('h3')
+    productName.className ='product-name'
+    productName.innerText = product.title
+
+const productPrices = document.createElement('p')
+    productPrices.className = 'product-prices'
+    productPrices.innerText = `$${product.price.toFixed(2)}`;
+
+
+    const cartIcon = document.createElement('svg')
+    cartIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+    cartIcon.setAttribute('width', '18')
+    cartIcon.setAttribute('height', '16') 
+    cartIcon.setAttribute('fill', 'currentColor')
+    cartIcon.setAttribute('class', 'bi bi-bag-plus')
+    cartIcon.setAttribute('viewBox', '0 0 16 16')
+
+    const path = document.createElement('path')
+    path.setAttribute(
+      'd',
+      'M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z'
+    );
+
+    cartIcon.appendChild(path)
+
+    // Agregar el ícono al elemento de precios
+    productPrices.appendChild(cartIcon)
+
+
+    productCard.appendChild(productImg)
+    productCard.appendChild(productName)
+    productCard.appendChild(productCategory)
+    productCard.appendChild(productPrices)
+
+    electronicsProductsContainer.appendChild(productCard)
 })
 }
 
